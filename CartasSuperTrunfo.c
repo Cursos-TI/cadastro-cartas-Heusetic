84b5954437147454b056645315ac1d460d0c0c1d
Heusetic <matheusmagnosilva19@gmail.com>
2026-02-10 03:56:23 +0000

    corrigindo cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
 
- printf("desafio do super trunfo")
- 
 int main() {
-  //Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+ 
 
-  // Área para exibição dos dados da cidade
+  char estado1;
+  char código1[5];
+  char cidade1[50]; 
+  int populacao1; 
+  float area1;
+  float pib1;
+  int pontos;
 
-return 0;
-} 
+  
+  char estado2[6]; 
+  char código2[5];
+  char cidade2[50]; 
+  int populacao2;
+  float area2;
+  float pib2;
+  int pontos2;
+
+  printf("Cadastro carta 1\n");
+  
+  printf("estado :");
+  scanf(" %c", &estado1);
+
+  printf("código :");
+  scanf("%s\n", &código1);
+
+  printf("cidade :");
+  scanf("%s", &cidade1);
+
+  printf("populacao :");
+  scanf("%d", &populacao1);
+
+  printf("area :");
+  scanf("%f", &area1);
+
+  printf("pib :");
+  scanf("%f", &pib1);
+  return 0;
+
+}  
