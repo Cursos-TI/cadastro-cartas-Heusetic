a8089d360870f811ca04effd8dc262d7fd981e41
Heusetic <matheusmagnosilva19@gmail.com>
2026-02-10 20:00:23 +0000

    coloquei as duas cartas para cadastro e esta ok

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,16 +4,16 @@ int main() {
 
  
 
-  char estado1;
+  char estado1[50];
   char código1[5];
   char cidade1[50]; 
   int populacao1; 
   float area1;
   float pib1;
-  int pontos;
+  int pontos1;
 
   
-  char estado2[6]; 
+  char estado2[50]; 
   char código2[5];
   char cidade2[50]; 
   int populacao2;
@@ -24,10 +24,10 @@ int main() {
   printf("Cadastro carta 1\n");
   
   printf("estado :");
-  scanf(" %c", &estado1);
+  scanf("%s", &estado1);
 
   printf("código :");
-  scanf("%s\n", &código1);
+  scanf("%s", &código1);
 
   printf("cidade :");
   scanf("%s", &cidade1);
@@ -40,6 +40,32 @@ int main() {
 
   printf("pib :");
   scanf("%f", &pib1);
+
+  printf("pontos turisticos :");
+  scanf(" %d", &pontos1);
+
+  printf("Cadastro carta 2\n");
+  
+  printf("estado :");
+  scanf("%s", &estado2);
+
+  printf("código :");
+  scanf("%s", &código2);
+
+  printf("cidade :");
+  scanf("%s", &cidade2);
+
+  printf("populacao :");
+  scanf("%d", &populacao2);
+
+  printf("area :");
+  scanf("%f", &area2);
+
+  printf("pib :");
+  scanf("%f", &pib2);
+
+  printf("pontos turisticos :");
+  scanf(" %d", &pontos2);
   return 0;
 
 }  
